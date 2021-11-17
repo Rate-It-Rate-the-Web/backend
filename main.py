e1c24b7fc2426cb08be812483a6aef7638f7f4d4
1NO0B <39493421+1NO0B@users.noreply.github.com>
2021-11-17 01:30:29 +0100

    .

--- main.py
+++ main.py
@@ -34,3 +34,24 @@ def postLike():
 def postDislike():
     url = request.args.get('url')
     loggedIn = session.get('logged_in')
+
+
+
+
+
+
+    """
+    >>> import sqlite3
+>>> con = sqlite3.connect("rateIt.db")
+>>> cur = con.cursor()
+>>> cur.execute('''CREATE TABLE URLs(url text)''')
+<sqlite3.Cursor object at 0x7ffb8ab34a40>
+>>> cur.execute('''CREATE TABLE likes(url text, likes int, dislikes int)''')
+<sqlite3.Cursor object at 0x7ffb8ab34a40>
+>>> cur.execute('''CREATE TABLE userRatings(url text, likeOrDislike text)''')
+<sqlite3.Cursor object at 0x7ffb8ab34a40>
+>>> 
+KeyboardInterrupt
+>>> con.commit()
+>>> con.close()
+"""
